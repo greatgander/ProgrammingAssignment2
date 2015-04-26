dfe40bbb3f24401cc61122ee313f57e7139baa38
unknown <EdwardT@EDWARDT.europe.EASYJET.LOCAL>
2015-04-26 19:33:29 +0100

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix creates list of function to set, get, setInverse and getInverse as req'd
 
-## Write a short comment describing this function
+## assign x matrix class
+makeCacheMatrix <- function(x = matrix())   {
+    
+    ## make null matrix to host inverse
+    Inv <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
+    ## set function created to assign y to x
+    set <- function(y)  {
+    ## assign y argument to x  
+      x <<- y
+      y
+    
+    ## reset I to null if x redefined
+      Inv <<- NULL
+                        }
+    ## get matrix, argument free function
+    get <- function() x
+  
+    ## sets the inverse using solve (inverse) function
+    setInverse <- function(solve) Inv <<- solve
+  
+    ## retrieves inverse, argument free function
+    getInverse <- function() Inv
+ 
+  ## makes list of the functions created above
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    
+                                          }
+## cacheSolve function checks to see whether inverse of matrix has already been calculated and retrieves this if so
+## else calculates the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  
+  ## retrieve the Inverse function created by setInverse above
+  Inv <- x$getInverse()
+  
+  ## if inverse has been created then Inv will not be null, and is retrieved message returned stating this, inverse returned 
+  if(!is.null(Inv)) {
+    message("getting cached data")
+    return(Inv)
+  
+                    }
+  ## if inverse has not been created then Inv will be null, and x is passed to solve function so calculate inverse
+  message("newly calculating data")
+  data <- x$get()
+  Inv <- solve(data, ...)
+  
+  ## inverse is set
+  x$setInverse(Inv)
+  
+  ## new inverse value returned
+  Inv
+                    }
+
